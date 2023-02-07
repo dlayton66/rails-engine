8d89f399d39a32791dc7428d21d6daa7f1391f14
Drew Layton <dlayton66@gmail.com>
2023-02-06 21:30:10 -0700

    feat/test: Get one merchant

--- spec/requests/api/v1/merchants/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants/merchants_request_spec.rb
@@ -26,4 +26,26 @@ RSpec.describe 'Merchants API' do
       expect(merchant[:updated_at]).to be_a(String)
     end
   end
+
+  it 'can get a merchant by id' do
+    id = create(:merchant).id
+    
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to eq(id)
+
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)
+
+    expect(merchant).to have_key(:created_at)
+    expect(merchant[:created_at]).to be_a(String)
+
+    expect(merchant).to have_key(:updated_at)
+    expect(merchant[:updated_at]).to be_a(String)
+  end
 end
