d1e9bc0ca0aabf8acb4de08171026d1550e11b17
Drew Layton <dlayton66@gmail.com>
2023-02-07 15:08:59 -0700

    feat/test: Get one merchant's items

--- spec/requests/api/v1/merchants/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants/merchants_request_spec.rb
@@ -28,16 +28,16 @@ RSpec.describe 'Merchants API' do
   end
 
   it 'can get a merchant by id' do
-    id = create(:merchant).id
+    merchant_id = create(:merchant).id
     
-    get "/api/v1/merchants/#{id}"
+    get "/api/v1/merchants/#{merchant_id}"
 
     expect(response).to be_successful
 
     merchant = JSON.parse(response.body, symbolize_names: true)[:data]
 
     expect(merchant).to have_key(:id)
-    expect(merchant[:id]).to eq(id.to_s)
+    expect(merchant[:id]).to eq(merchant_id.to_s)
 
     expect(merchant).to have_key(:type)
     expect(merchant[:type]).to be_a(String)
@@ -52,12 +52,37 @@ RSpec.describe 'Merchants API' do
   it 'can get all items from a merchant' do
     merchant_id = create(:merchant).id
     merchant_items = create_list(:item, 3, merchant_id: merchant_id)
-    other_merchant_items = create_list(:item, 3)
+    other_merchant_item = create(:item)
 
     get "/api/v1/merchants/#{merchant_id}/items"
 
     expect(response).to be_successful
 
-    items = JSON.parse(response.body, symbolize_names: true)
+    items = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_a(String)
+      expect(item[:id]).to_not eq(other_merchant_item.id.to_s)
+
+      expect(item).to have_key(:type)
+      expect(item[:type]).to be_a(String)
+
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes]).to be_a(Hash)
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_an(Integer)
+      expect(item[:attributes][:merchant_id]).to eq(merchant_id)
+    end
   end
 end
