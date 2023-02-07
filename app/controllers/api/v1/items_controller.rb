32ec66debdd6e0150b926286eb5fe3949b0bbc76
Drew Layton <dlayton66@gmail.com>
2023-02-07 15:35:51 -0700

    feat/test: Get one item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,4 +2,15 @@ class Api::V1::ItemsController < ApplicationController
   def index
     render json: ItemSerializer.new(Item.all)
   end
+
+  def show
+    get_item
+    render json: ItemSerializer.new(@item)
+  end
+
+  private
+  
+    def get_item
+      @item = Item.find(params[:id])
+    end
 end
--- spec/requests/api/v1/items/items_request_spec.rb
+++ spec/requests/api/v1/items/items_request_spec.rb
@@ -36,4 +36,37 @@ RSpec.describe 'Items API' do
     end
 
   end
+
+  it 'can get one item' do
+    item_id = create(:item).id
+
+    get "/api/v1/items/#{item_id}"
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(item).to be_a(Hash)
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to eq(item_id.to_s)
+
+    expect(item).to have_key(:type)
+    expect(item[:type]).to be_a(String)
+
+    expect(item).to have_key(:attributes)
+    expect(item[:attributes]).to be_a(Hash)
+    
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_a(Float)
+
+    expect(item[:attributes]).to have_key(:merchant_id)
+    expect(item[:attributes][:merchant_id]).to be_an(Integer)
+  end
 end
